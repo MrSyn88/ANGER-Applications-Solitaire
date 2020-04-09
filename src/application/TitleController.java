0859e9190e2263e66dfa2d603d74fd466780f261
gglyssy <60710579+gglyssy@users.noreply.github.com>
2020-04-09 17:47:24 -0500

    New edits to title.fxml/titlecontroller

---
+++ src/application/TitleController.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TitleController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button NewGamePush;
+
+    @FXML
+    private Button LeaderboardPush;
+
+    @FXML
+    private Button HTPPush;
+
+    @FXML
+    private Button SettingsPush;
+
+    @FXML
+    void initialize() {
+        
+
+    }
+}
