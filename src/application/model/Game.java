9a11b54a0201d9bc2e0be0e58fea7fc1e0e18f62
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-04-24 15:54:55 -0500

    Added more methods to the game class. These are by no means final; I expect to need to change them in order to get things working on the graphics side.

--- src/application/model/Game.java
+++ src/application/model/Game.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Game {
 	private Deck gameDeck;
 	private List<Deque<Card>> foundations;
 	private Deque<Card> draw;
 	private Deque<Card> tempCardStack;
+	private Deque<Card> drawDiscardStack;
+
+	private IntegerProperty drawNumber;
 
 	private List<Deque<Card>> playArea;
 
@@ -25,6 +31,7 @@ public class Game {
 
 		// Add a new Deque for the draw pile
 		this.draw = new ArrayDeque<Card>();
+		this.drawDiscardStack = new ArrayDeque<Card>();
 		// Add a new Deque to hold cards that are being moved
 		this.tempCardStack = new ArrayDeque<Card>();
 
@@ -33,6 +40,8 @@ public class Game {
 		for (int i = 0; i < 7; i++) {
 			this.playArea.add(i, new ArrayDeque<Card>(i + 1));
 		}
+		this.drawNumber = new SimpleIntegerProperty();
+		this.setDrawNumber(3);
 	}
 
 	public void startNewGame() {
@@ -47,6 +56,69 @@ public class Game {
 			}
 		}
 		this.draw.addAll(gameDeck);
+	}
+
+	/**
+	 * Function that draws the next card from the top of the draw pile
+	 * 
+	 */
+	public void drawNextCard() {
+		// If the draw pile is empty, then move all cards from the discard stack to the
+		// draw stack
+		if (this.draw.isEmpty()) {
+			for (Card currentCard : this.drawDiscardStack) {
+				currentCard.setIsFaceUp(false);
+				this.draw.push(currentCard);
+			}
+			this.drawDiscardStack.clear();
+			return;
+		}
+
+		// Otherwise, move up to the draw number of cards from the draw stack to the
+		// draw discard stack
+		for (int i = 0; i < this.getDrawNumber() && !this.draw.isEmpty(); i++) {
+			this.draw.peek().setIsFaceUp(true);
+			this.drawDiscardStack.push(this.draw.pop());
+		}
+	}
+
+	/**
+	 * @param cardToMove
+	 * @param sourceColumn
+	 * @param destColumn
+	 */
+	public void moveCardToStack(Card cardToMove, Integer sourceColumn, Integer destColumn) {
+
+		Deque<Card> source = this.playArea.get(sourceColumn);
+		Deque<Card> dest = this.playArea.get(destColumn);
+		// Can't move a card if it's not face up
+		if (!cardToMove.getIsFaceUp() || !cardToMove.isValidTableCard(dest.peek())) {
+			return;
+		}
+
+		while (!source.peek().equals(cardToMove)) {
+			this.tempCardStack.push(source.pop());
+		}
+		dest.push(cardToMove);
+		for (Card currentCard : this.tempCardStack) {
+			dest.push(currentCard);
+		}
+		this.tempCardStack.clear();
+	}
+
+	public void moveCardToFoundation(Card cardToMove, Integer sourceColumn, Integer destFoundation) {
+		Deque<Card> source = this.playArea.get(sourceColumn);
+		Deque<Card> dest = this.foundations.get(destFoundation);
+		// Can't move a card if it's not face up
+		if (!cardToMove.getIsFaceUp() || !cardToMove.isValidFoundationCard(dest.peek())) {
+			return;
+		} else if (source.peek() != cardToMove) {
+			return;
+		}
+	}
+
+	public void moveCardFromDraw(Card cardToMove) {
+
 	}
 
 	/**
@@ -82,6 +154,55 @@ public class Game {
 	 */
 	public List<Deque<Card>> getFoundations() {
 		return foundations;
+	}
+
+	/**
+	 * @return the drawDiscardStack
+	 */
+	public Deque<Card> getDrawDiscardStack() {
+		return drawDiscardStack;
+	}
+
+	/**
+	 * @param drawDiscardStack the drawDiscardStack to set
+	 */
+	public void setDrawDiscardStack(Deque<Card> drawDiscardStack) {
+		this.drawDiscardStack = drawDiscardStack;
+	}
+
+	/**
+	 * @return the tempCardStack
+	 */
+	public Deque<Card> getTempCardStack() {
+		return tempCardStack;
+	}
+
+	/**
+	 * @return the playArea
+	 */
+	public List<Deque<Card>> getPlayArea() {
+		return playArea;
+	}
+
+	/**
+	 * @return the drawNumber
+	 */
+	public Integer getDrawNumber() {
+		return drawNumber.getValue();
+	}
+
+	/**
+	 * @param drawNumber the drawNumber to set
+	 */
+	public void setDrawNumber(Integer drawNumber) {
+		this.drawNumber.setValue(drawNumber);
+	}
+
+	public IntegerProperty getDrawNumberProperty() {
+		if (this.drawNumber == null) {
+			this.drawNumber = new SimpleIntegerProperty();
+		}
+		return this.drawNumber;
 	}
 
 }
